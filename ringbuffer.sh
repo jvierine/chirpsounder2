9eaaa71679b239c14d03a192ba08bafe9ccaece0
Juha <jvierine@gmail.com>
2020-10-03 22:57:23 +0000

    added realtime capability for detection

--- ringbuffer.sh
+++ ringbuffer.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
 
-drf ringbuffer -z 4000MB /dev/shm/hf25 -p 2 &
-thor.py -m 192.168.10.3 -d A:A -c cha -f 12.5e6 -r 25e6 /dev/shm/hf25 
+
+thor.py -m 192.168.10.3 -d A:A -c cha -f 12.5e6 -r 25e6 /dev/shm/hf25 &
+drf ringbuffer -z 2000MB /dev/shm/hf25 -p 2 
