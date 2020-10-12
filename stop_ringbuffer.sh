62e7eddc3cd3c4985b56e6bfcad1955e301a96e8
Juha <jvierine@gmail.com>
2020-10-12 10:49:18 +0000

    added script to stop all processes

---
+++ stop_ringbuffer.sh
@@ -0,0 +1,9 @@
+
+# stop all processes doing realtime chirp analysis
+kill `ps ax |grep detect_chirps |grep python |gawk '{print $1}'|xargs`
+kill `ps ax |grep thor.py|grep python |gawk '{print $1}' |xargs`
+kill `ps ax |grep plot_ionograms.py|grep python |gawk '{print $1}' |xargs`
+kill `ps ax |grep calc_ionograms.py|grep python |gawk '{print $1}' |xargs`
+kill `ps ax |grep drf|grep python |gawk '{print $1}' |xargs`
+# remove remaining data in ringbuffer
+rm -Rf /dev/shm/hf25
