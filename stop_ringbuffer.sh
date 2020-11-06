3afa84dab3697c159b18aed28b71d31065919404
Juha <jvierine@gmail.com>
2020-11-06 00:04:54 +0000

    added stopping of find_timings

--- stop_ringbuffer.sh
+++ stop_ringbuffer.sh
@@ -1,6 +1,7 @@
 
 # stop all processes doing realtime chirp analysis
 kill `ps ax |grep detect_chirps |grep python |gawk '{print $1}'|xargs`
+kill `ps ax |grep find_timings |grep python |gawk '{print $1}'|xargs`
 kill `ps ax |grep thor.py|grep python |gawk '{print $1}' |xargs`
 kill `ps ax |grep plot_ionograms.py|grep python |gawk '{print $1}' |xargs`
 kill `ps ax |grep calc_ionograms.py|grep python |gawk '{print $1}' |xargs`
