f323fb2e47cef679f9c636d989432b220d06510d
Juha Vierinen <jvierine@gmail.com>
2021-06-26 01:06:05 +0200

    absolute path

--- examples/sgo/sgo.sh
+++ examples/sgo/sgo.sh
@@ -2,19 +2,27 @@
 #
 # start a ringbuffer
 #
+CONFFILE=/home/chrpsdr/src/chirpsounder2/examples/sgo/sgo.ini
+DDIR=/dev/shm/hf25
 # sync to ntp time
 sudo ntpdate ntp.uit.no
-# start digital rf acquisition
-thor.py -m 192.168.10.2 -d A:A -c cha -f 12.5e6 -r 25e6 /dev/shm/hf25 &
-sleep 10
+
 # setup ringbuffer
-drf ringbuffer -z 1700MB /dev/shm/hf25 -p 2 &
-sleep 10
+drf ringbuffer -z 1700MB $DDIR -p 2 &
+
 
 # Calculate ionograms using known timings
 # use two parallel threads. one for SGO and one for HAARP
-mpirun.mpich -np 2 python calc_ionograms.py sgo.ini &
+python3 calc_ionograms.py $CONFFILE &
 sleep 10
 
 # plot ionograms
-python plot_ionograms.py conf/sgo.ini
+python3 plot_ionograms.py $CONFFILE &
+
+while true;
+do
+    # start digital rf acquisition
+    thor.py -m 192.168.10.2 -d A:A -c cha -f 12.5e6 -r 25e6 $DDIR 
+    sleep 10
+done
+    
