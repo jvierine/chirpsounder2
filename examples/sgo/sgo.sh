3a73a72295f091306fd60628184299505ae48ec7
Juha Vierinen <jvierine@gmail.com>
2022-06-07 17:02:56 +0200

    fixed skibotn bug

--- examples/sgo/sgo.sh
+++ examples/sgo/sgo.sh
@@ -2,11 +2,11 @@
 #
 # start a ringbuffer
 #
-INSTALL_PATH=/home/hfrx3/src/chirpsounder2
+INSTALL_PATH=/home/hfrx2/src/chirpsounder2
 cd $INSTALL_PATH
 # stop all processes
 ./stop_ringbuffer.sh
-CONFFILE=/home/hfrx3/src/chirpsounder2/examples/sgo/sgo.ini
+CONFFILE=/home/hfrx2/src/chirpsounder2/examples/sgo/sgo.ini
 DDIR=/dev/shm/hf25
 mkdir -p logs
 
@@ -20,17 +20,18 @@ mkdir -p $DDIR
 
 # setup ringbuffer
 echo "Ringbuffer"
-drf ringbuffer -z 30000MB $DDIR -p 2 >logs/ringbuffer.log 2>&1 &
+nohup drf ringbuffer -z 30000MB $DDIR -p 2 >logs/ringbuffer.log 2>&1 &
 
 
 # Calculate ionograms using known timings
 # use two parallel threads. one for SGO and one for HAARP
 echo "Ionogram calc"
-python3 calc_ionograms.py $CONFFILE >logs/calc_ionograms.log 2>&1 &
+nohup python3 calc_ionograms.py $CONFFILE >logs/calc_ionograms.log 2>&1 &
 sleep 10
 
 # plot ionograms
 echo "Plot ionograms"
-python3 plot_ionograms.py $CONFFILE >logs/plot_ionograms.log 2>&1 &
+nohup python3 plot_ionograms.py $CONFFILE >logs/plot_ionograms.log 2>&1 &
 
-bash rx_uhd_wrapper.sh &
+# disown
+nohup bash rx_uhd_wrapper.sh &
