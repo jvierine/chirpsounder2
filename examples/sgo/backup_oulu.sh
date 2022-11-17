cedd836b7c62225add135eafcc96ee8000be0035
Juha Vierinen <jvierine@gmail.com>
2022-11-17 16:19:00 +0100

    aasd

--- examples/sgo/backup_oulu.sh
+++ examples/sgo/backup_oulu.sh
@@ -1,1 +1,4 @@
-rsync -e "ssh -i ~/.ssh/id_rsa" -av --progress /data0/2* j@mikromikko.sgo.fi:noire/oul/
+while true; do
+    rsync -e "ssh -i ~/.ssh/id_rsa" -av --progress /data0/2* j@mikromikko.sgo.fi:noire/oul/
+    sleep 120
+done
